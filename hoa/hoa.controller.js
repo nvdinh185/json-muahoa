e257ca8f8f1aa56e1091e5bf58b7c373f6c5badb
nvdinh185 <nvdinh0766777123@gmail.com>
2023-03-31 17:34:12 +0700

    x

--- hoa/hoa.controller.js
+++ hoa/hoa.controller.js
@@ -5,48 +5,12 @@ const hoaHandler = require('./hoa.handler');
 const hoaMiddleware = require('./hoa.middleware');
 
 // routes
-router.get('/', getListHoa);
-router.get('/type', getListTypeHoa);
-router.get('/hoa', getHoaById);
+router.get('/', hoaHandler.getListHoa);
+router.get('/type', hoaHandler.getListTypeHoa);
+router.get('/hoa', hoaHandler.getHoaById);
 
-router.post('/add', hoaMiddleware.uploadFile, postAddHoa);
-router.post('/edit', hoaMiddleware.uploadFile, postEditHoa);
-router.post('/delete', postDeleteHoa);
+router.post('/add', hoaMiddleware.uploadFile, hoaHandler.postAddHoa);
+router.post('/edit', hoaMiddleware.uploadFile, hoaHandler.postEditHoa);
+router.post('/delete', hoaHandler.postDeleteHoa);
 
 module.exports = router;
-
-function getListHoa(req, res, next) {
-    hoaHandler.getListHoa()
-        .then(listHoa => res.json(listHoa))
-        .catch(err => next(err));
-}
-
-function getListTypeHoa(req, res, next) {
-    hoaHandler.getListTypeHoa()
-        .then(listHoa => res.json(listHoa))
-        .catch(err => next(err));
-}
-
-function getHoaById(req, res, next) {
-    hoaHandler.getHoaById(req.query)
-        .then(hoaById => res.json(hoaById))
-        .catch(err => next(err));
-}
-
-function postAddHoa(req, res, next) {
-    hoaHandler.postAddHoa(req.form_data)
-        .then(result => res.json(result))
-        .catch(err => next(err));
-}
-
-function postEditHoa(req, res, next) {
-    hoaHandler.postEditHoa(req.form_data)
-        .then(result => res.json(result))
-        .catch(err => next(err));
-}
-
-function postDeleteHoa(req, res, next) {
-    hoaHandler.postDeleteHoa(req.body)
-        .then(result => res.json(result))
-        .catch(err => next(err));
-}
--- hoa/hoa.handler.js
+++ hoa/hoa.handler.js
@@ -3,9 +3,6 @@ const jwt = require('jsonwebtoken');
 
 const sqlite3 = require('sqlite3').verbose();
 const dbFile = './database/shophoa.db';
-const db = new sqlite3.Database(dbFile);
-
-db.serialize();
 
 module.exports = {
     getListHoa,
@@ -16,67 +13,120 @@ module.exports = {
     postDeleteHoa
 }
 
-async function getListHoa() {
-    const listHoa = await new Promise((resolve, reject) => {
-        db.all(`SELECT * FROM hoa JOIN loaihoa ON hoa.type = loaihoa.type_id`, (err, row) => {
-            if (err) reject(err);
-            resolve(row);
+async function getListHoa(req, res, next) {
+    try {
+        var db = new sqlite3.Database(dbFile);
+        db.serialize();
+        const listHoa = await new Promise((resolve, reject) => {
+            db.all(`SELECT * FROM hoa JOIN loaihoa ON hoa.type = loaihoa.type_id`, (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            })
         })
-    })
-    return listHoa;
+        res.json(listHoa);
+    } catch (err) {
+        next(err);
+    } finally {
+        db.close();
+    }
 }
 
-async function getListTypeHoa() {
-    const listHoaType = await new Promise((resolve, reject) => {
-        db.all(`SELECT * FROM loaihoa`, (err, row) => {
-            if (err) reject(err);
-            resolve(row);
+async function getListTypeHoa(req, res, next) {
+    try {
+        var db = new sqlite3.Database(dbFile);
+        db.serialize();
+        const listHoaType = await new Promise((resolve, reject) => {
+            db.all(`SELECT * FROM loaihoa`, (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            })
         })
-    })
-    return listHoaType;
+        res.json(listHoaType);
+    } catch (err) {
+        next(err);
+    } finally {
+        db.close();
+    }
 }
 
-async function getHoaById(query) {
-    const hoaById = await new Promise((resolve, reject) => {
-        db.each(`SELECT * FROM hoa JOIN loaihoa ON hoa.type = loaihoa.type_id WHERE hoa.id = ${query.id}`, (err, row) => {
-            if (err) reject(err);
-            resolve(row);
+async function getHoaById(req, res, next) {
+    try {
+        var db = new sqlite3.Database(dbFile);
+        db.serialize();
+        const hoaById = await new Promise((resolve, reject) => {
+            db.each(`SELECT * FROM hoa JOIN loaihoa ON hoa.type = loaihoa.type_id WHERE hoa.id = ${req.query.id}`, (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            })
         })
-    })
-    return hoaById;
+        res.json(hoaById);
+    } catch (err) {
+        next(err);
+    } finally {
+        db.close();
+    }
 }
 
-async function postAddHoa(formData) {
-    return new Promise((resolve, reject) => {
-        db.run(`INSERT INTO hoa (name, type, amount, price, image) VALUES (?, ?, ?, ?, ?)`,
-            [formData.ten, formData.loaihoa, formData.soluong, formData.giaban, formData.file], function (err) {
+async function postAddHoa(req, res, next) {
+    var formData = req.form_data;
+    try {
+        var db = new sqlite3.Database(dbFile);
+        db.serialize();
+        var result = await new Promise((resolve, reject) => {
+            db.run(`INSERT INTO hoa (name, type, amount, price, image) VALUES (?, ?, ?, ?, ?)`,
+                [formData.ten, formData.loaihoa, formData.soluong, formData.giaban, formData.file], function (err) {
+                    if (err) {
+                        reject(new Error(err.message));
+                    }
+                    resolve(this.changes);
+                });
+        })
+        res.json(result);
+    } catch (err) {
+        next(err);
+    } finally {
+        db.close();
+    }
+}
+
+async function postEditHoa(req, res, next) {
+    var formData = req.form_data;
+    try {
+        var db = new sqlite3.Database(dbFile);
+        db.serialize();
+        var result = await new Promise((resolve, reject) => {
+            db.run(`UPDATE hoa SET name = '${formData.ten}', type = '${formData.loaihoa}', amount = '${formData.soluong}',
+        price = '${formData.giaban}', image = "${formData.file ? formData.file : 'image'}" WHERE id = '${formData.id}'`, function (err) {
                 if (err) {
                     reject(new Error(err.message));
                 }
                 resolve(this.changes);
             });
-    })
+        })
+        res.json(result);
+    } catch (err) {
+        next(err);
+    } finally {
+        db.close();
+    }
 }
 
-async function postEditHoa(formData) {
-    return new Promise((resolve, reject) => {
-        db.run(`UPDATE hoa SET name = '${formData.ten}', type = '${formData.loaihoa}', amount = '${formData.soluong}',
-        price = '${formData.giaban}', image = "${formData.file ? formData.file : 'image'}" WHERE id = '${formData.id}'`, function (err) {
-            if (err) {
-                reject(new Error(err.message));
-            }
-            resolve(this.changes);
-        });
-    })
-}
-
-async function postDeleteHoa(body) {
-    return new Promise((resolve, reject) => {
-        db.run(`DELETE FROM hoa WHERE id = ${body.id}`, function (err) {
-            if (err) {
-                reject(new Error(err.message));
-            }
-            resolve(this.changes);
-        });
-    })
+async function postDeleteHoa(req, res, next) {
+    try {
+        var db = new sqlite3.Database(dbFile);
+        db.serialize();
+        var result = await new Promise((resolve, reject) => {
+            db.run(`DELETE FROM hoa WHERE id = ${req.body.id}`, function (err) {
+                if (err) {
+                    reject(new Error(err.message));
+                }
+                resolve(this.changes);
+            });
+        })
+        res.json(result);
+    } catch (err) {
+        next(err);
+    } finally {
+        db.close();
+    }
 }
